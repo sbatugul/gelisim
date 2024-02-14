fad698157912a005a4eaa3b6b287d347975f69a8
sbatugul <sbatuhuangul@hotmail.com>
2024-02-14 17:20:28 +0300

    branch data update

--- app/lib/branch-data.js
+++ app/lib/branch-data.js
@@ -2,10 +2,51 @@ const branch = [
   {
     id: "turgutlu",
     name: "turgutlu",
+    insta: "https://www.instagram.com/hizligelisimturgutlu",
+    facebook: "https://www.facebook.com/hizligelisim",
+    educations: [
+      {
+        name: "Otizm Spektrum Bozukluğu Programı",
+      },
+      {
+        name: "Zihinsel Yetersizlik Programı",
+      },
+      {
+        name: "Bedensel Yetersizlik Programı ",
+      },
+      {
+        name: "Öğrenme Güçlüğü Programı ",
+      },
+      {
+        name: "İşitme Yetersizliği Programı ",
+      },
+    ],
   },
   {
     id: "salihli",
-    name: "salihli",
+    name: "Özel Hızlı Gelişim Özel Eğitim ve Rehabilitasyon Merkezi Salihli ",
+    insta: "https://www.instagram.com/hizligelisim",
+    facebook: "https://www.facebook.com/hizligelisim",
+    educations: [
+      {
+        name: "Otizm Spektrum Bozukluğu Programı",
+      },
+      {
+        name: "Dil ve Konuşma Bozukluğu Programı",
+      },
+      {
+        name: "Zihinsel Yetersizlik Programı",
+      },
+      {
+        name: "Bedensel Yetersizlik Programı ",
+      },
+      {
+        name: "Öğrenme Güçlüğü Programı ",
+      },
+      {
+        name: "İşitme Yetersizliği  Programı ",
+      },
+    ],
   },
 ];
 
