b2ba12b64dda049216c041f3ebff3232d27d04b2
sbatugul <sbatuhuangul@hotmail.com>
2024-03-16 12:45:33 +0300

    implement education card

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        "500": "500px",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
